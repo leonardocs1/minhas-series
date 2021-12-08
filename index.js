bcd8935a0d084df4e06a859ca48b385f601b11a4
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-12-08 17:38:48 -0300

    Iniciando o projeto

---
+++ index.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+const mongo = process.env.MONGODB || 'mongodb://localhost/minhas-series'
+
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+// process request body
+app.use(bodyParser.urlencoded({extended: true}))
+
+// assets
+app.use(express.static('public'))
+
+// view engine - EJS
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.send('ok'))
+
+mongoose
+.connect(mongo)
+.then(() => {
+  app.listen(port, () => console.log('Listening on: ' + port))
+})
+.catch(e => {
+  console.log(e)
+})
