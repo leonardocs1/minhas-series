36d25832c4ae2bf05874638f6f7bd49a6e814bfd
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-12-09 20:09:11 -0300

    Excluir, editar e organizar a lista de séries

--- controllers/series.js
+++ controllers/series.js
@@ -1,6 +1,12 @@
+const labels = [
+  {id: 'to-watch', name: 'Para assistir'},
+  {id: 'watching', name: 'Assistindo'},
+  {id: 'watched', name: 'Assistido'}
+]
+
 const index = ({ Serie }, req, res) => {
   Serie.find({}, (err, docs) => {
-    res.render('series/index', {series: docs})
+    res.render('series/index', {series: docs, labels})
   })
 }
 
@@ -15,6 +21,29 @@ const novaForm = (req, res) => {
   res.render('series/nova')
 }
 
+const excluir = ({ Serie }, req, res) => {
+  Serie.remove({
+    _id: req.params.id
+  }, (err) => {
+    res.redirect('/series')
+  })
+}
+
+const editarProcess = ({ Serie }, req, res) => {
+  Serie.findOne({_id: req.params.id}, (err, serie) => {
+    serie.name = req.body.name,
+    serie.status = req.body.status
+    serie.save()
+    res.redirect('/series')
+  })
+}
+
+const editarForm = ({ Serie }, req, res) => {
+  Serie.findOne({_id: req.params.id}, (err, serie) => {
+    res.render('series/editar', { serie, labels })
+  })
+}
+
 module.exports = {
-  index, novaProcess, novaForm
+  index, novaProcess, novaForm, excluir, editarForm, editarProcess
 }
